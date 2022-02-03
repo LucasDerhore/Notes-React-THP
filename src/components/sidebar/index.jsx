7ea9f4c0e83e9e3ebba2f51bae375592c384d612
lucasd <derhore.lucas@gmail.com>
2022-02-03 12:04:36 +0100

    First and final commit

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,46 @@
+const Sidebar = ({
+  notes,
+  onAddNote,
+  onDeleteNote,
+  activeNote,
+  setActiveNote,
+}) => {
+  const sortedNotes = notes.sort((a, b) => b.lastModified - a.lastModified);
+
+  return (
+    <div className="app-sidebar">
+      <div className="app-sidebar-header">
+        <h1>Notes</h1>
+        <button className="Addbutton" onClick={onAddNote}>
+          Ajoute une note
+        </button>
+      </div>
+      <div className="app-sidebar-notes">
+        {sortedNotes.map(({ id, title, body, lastModified }, i) => (
+          <div
+            className={`app-sidebar-note ${id === activeNote && "active"}`}
+            onClick={() => setActiveNote(id)}
+          >
+            <div className="sidebar-note-title">
+              <strong>{title}</strong>
+              <button className="Deletbutton" onClick={(e) => onDeleteNote(id)}>
+                Supprimer
+              </button>
+            </div>
+            {/* Affiche max 100 caractère + ... si ca dépasse */}
+            <p>{body && body.substr(0, 100) + "..."}</p>
+            <small className="note-meta">
+              Dernière modification{" "}
+              {new Date(lastModified).toLocaleDateString("en-GB", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </small>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
